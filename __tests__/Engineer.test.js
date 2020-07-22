4dd6d045c137c94775a94eeb323e3cf4510db078
shhu21 <hu.sara21@gmail.com>
2020-07-21 17:25:00 -0700

    updates engineer test

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,11 +1,12 @@
 const Engineer = require('../lib/Engineer');
 
 test('creates an Engineer object', () => {
-    const engineer = new Engineer('Dave', 1, 'dave@gmail.com');
+    const engineer = new Engineer('Dave', 1, 'dave@gmail.com', 'github.com');
   
     expect(engineer.name).toBe('Dave');
     expect(engineer.name).toEqual(expect.any(String));
     expect(engineer.id).toEqual(expect.any(Number));
     expect(engineer.email).toEqual(expect.any(String));
+    expect(engineer.github).toEqual(expect.any(String));
     expect(engineer.getRole()).toEqual('Engineer');
 });
